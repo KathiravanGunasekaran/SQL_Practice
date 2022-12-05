549b80ebe6889af4f8b9e0e56689925c33db8a85
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-05 10:32:13 +0530

    Insert the values into Patients

---
+++ Insert_into_Patients.sql
@@ -0,0 +1,6 @@
+INSERT INTO Patients (ssn, PatientID, Name, Address, Phone, insuranceID, empID)
+VALUES (20101, 1,'Steve', 'US', 6386783254,2001, 1),
+(20102, 2 ,'Smith', 'UK', 6386783255, 2002,2),
+(20103, 3, 'Katherine', 'India', 6386783256,2003,2),
+(20104,4, 'Siva', 'India', 6385783257,2004,5),
+(20105,5, 'Janani', 'US', 6385783258,2005,3);
