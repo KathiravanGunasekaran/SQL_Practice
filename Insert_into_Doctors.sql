1f4dc8699703dff9deeb05dff2a6faec1697bb4e
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-05 10:30:54 +0530

    Insert the Values in Doctors Table

---
+++ Insert_into_Doctors.sql
@@ -0,0 +1,6 @@
+INSERT INTO Doctors (empID, Fname, Lname, Position, ssn)
+VALUES (1, 'John', 'Fernandes', 'Ortho', 1001),
+(2, 'Kedar', 'Santhosh', 'Cardio', 1002),
+(3, 'Sumit', 'Anshul', 'Psychiatrist', 1003),
+(4, 'Emily', 'Clock', 'Neurologist', 1004),
+(5, 'Stephen', 'Strange', 'Dean', 1000);
