b3719270e9c1b7d4ce8c30dc82df9d4032ac701a
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-05 10:29:26 +0530

    Creating Table named Patients
    With EmpID as Foreign key and PatientID as Primary key

---
+++ create_pat_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Patients (
+    ssn int,
+    PatientID int PRIMARY KEY,
+    Name varchar(255),
+    Address varchar(255),
+    Phone BIGINT,
+    insuranceId int,
+    empID int,
+    constraint FK_Doctor foreign key (empID) references Doctors (empID)
+);
