a624f1fe487e652e34a8d0d79d6cdbf09e1715b1
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-05 10:37:13 +0530

    Adding 3 rows in Doctor Table

---
+++ Adding_values.sql
@@ -0,0 +1,4 @@
+INSERT INTO Doctors (empID, Fname, Lname, Position, ssn)
+VALUES (6, 'Robert', 'Kennedy', 'Ortho', 1005),
+(7, 'Vikram', 'Prabhu', 'Ortho', 1006),
+(8, 'Vijeth', 'Joystick', 'Psychiatrist', 1007);
