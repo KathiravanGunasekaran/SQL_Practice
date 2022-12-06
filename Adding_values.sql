f7d34a4cda97c08e5947b4490050c9154dbbc190
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:46:14 +0530

    Update Adding_values.sql

--- Adding_values.sql
+++ Adding_values.sql
@@ -1,3 +1,5 @@
+--Create insert queries to add around 4 to 8 rows in both the tables
+
 INSERT INTO Doctors (empID, Fname, Lname, Position, ssn)
 VALUES (6, 'Robert', 'Kennedy', 'Ortho', 1005),
 (7, 'Vikram', 'Prabhu', 'Ortho', 1006),
