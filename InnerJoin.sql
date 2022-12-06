fbb7032357063c0f85fa23b91c56cea726703766
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:58:52 +0530

    Update InnerJoin.sql

--- InnerJoin.sql
+++ InnerJoin.sql
@@ -1,1 +1,10 @@
+--Prepare a INNER JOIN query between table1 and table2
+
 select CONCAT(d.Fname,' ',d.Lname) as DoctorName, p.Name as PatientName from Doctors d Inner Join Patients p on d.empID=p.empID; 
+
+--DoctorName  PatientName
+--John Fernandes	Steve
+--Kedar Santhosh	Smith
+--Kedar Santhosh	Katherine
+--Stephen Strange	Siva
+--Sumit Anshul	Janani
