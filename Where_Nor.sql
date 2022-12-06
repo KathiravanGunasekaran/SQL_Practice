ce4958e298f50e31338abfb794aa4d6e638b8d3a
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:49:22 +0530

    Update Where_Nor.sql

--- Where_Nor.sql
+++ Where_Nor.sql
@@ -1,1 +1,10 @@
+--Prepare a select query using WHERE, 'NOT IN', LIKE and ORDER BY clause
+
 SELECT Fname , Lname from Doctors where NOT Position = 'Ortho';
+
+--Fname   Lname
+--Kedar	  Santhosh
+--Sumit	  Anshul
+--Emily	  Clock
+--Stephen	Strange
+--Vijeth	Joystick
