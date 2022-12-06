42abf267418b3adcfa7df7815e0dff852566e0f1
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:53:02 +0530

    Update Where_Orderby.sql

--- Where_Orderby.sql
+++ Where_Orderby.sql
@@ -1,1 +1,7 @@
-select Fname, Lname from Doctors where Position ='Ortho' ORDER BY SSN; 
+--Prepare a select query using WHERE, 'NOT IN', LIKE and ORDER BY clause
+select Fname, Lname, SSN from Doctors where Position ='Ortho' ORDER BY SSN; 
+
+--Fname Lname     SSN
+--John	Fernandes	1001
+--Robert	Kennedy	1005
+--Vikram	Prabhu	1006
