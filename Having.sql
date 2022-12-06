834400e96e56e20d72a317eec2a5d2ca7c8f8392
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:56:52 +0530

    Update Having.sql

--- Having.sql
+++ Having.sql
@@ -1,1 +1,8 @@
+--Prepare a select query using GROUP BY and HAVING clause, with COUNT, SUM
 select Count(empID) as nOOfDoc ,Position from Doctors Group by Position HAVING Count(empID)>1;
+
+--nOOfDoc Position
+--3	Ortho
+--2	Psychiatrist
+
+
