b159c9e6b38d01e247ec741f12f3f3209ed37dde
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:51:21 +0530

    Update Like.sql

--- Like.sql
+++ Like.sql
@@ -1,1 +1,7 @@
-Select Fname from Doctors Where Lname Like 'S%';
+--Prepare a select query using WHERE, 'NOT IN', LIKE and ORDER BY clause
+
+Select Fname,Lname from Doctors Where Lname Like 'S%';
+
+--Fname Lname
+--Kedar	Santhosh
+--Stephen	Strange
