eae3b31de54d03bc79f34f311379f10563e7b76b
G.Kathiravan <Kathirguna0811@gmail.com>
2022-12-06 22:55:36 +0530

    Update Group_BY.sql

--- Group_BY.sql
+++ Group_BY.sql
@@ -1,1 +1,9 @@
-select Count(empID),Position from Doctors Group by Position;
+--Prepare a select query using GROUP BY and HAVING clause, with COUNT, SUM
+select Count(empID) as Count_doc ,Position from Doctors Group by Position;
+
+--count_doc position
+--1	Cardio
+--1	Dean
+--1	Neurologist
+--3	Ortho
+--2	Psychiatrist
